09795e8d7df64e531ddfaf5121d4be1def7a74f7
CM977CD <Elnura.Mirzayeva@gds.ey.com>
2025-03-31 15:40:38 +0200

    2\3 approach

--- src/myfirstPet.java
+++ src/myfirstPet.java
@@ -2,5 +2,6 @@ public class myfirstPet {
 
     public static void main(String[] args) {
         System.out.println("Let's change the code: ");
+        System.out.println("it's what I do!");
     }
 }
