380e5a1135428686d89fd19a801b244c6bc6e0c7
CM977CD <Elnura.Mirzayeva@gds.ey.com>
2025-04-01 15:05:27 +0200

    new2

---
+++ src/FavoriteBook.java
@@ -0,0 +1,17 @@
+public class FavoriteBook {public class FavoriteBook {
+    public static void main(String args[]) {
+
+
+        //TODO: Your code goes here
+        String title = "Idiot";
+        String autor = " Fiodor Dostoevskiy";
+        int page = 345;
+        double money = 56.99;
+
+        System.out.println();
+
+
+    }
+}
+
+}
